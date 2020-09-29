98840612d21fb670e82d1f01704ffcf0782eb9e6
Janak Patel <jpatel2@madisoncollege.edu>
2020-09-29 10:20:33 -0500

    this is week four 4 excerci just impliment in project

---
+++ src/main/java/hote/entity/User.java
@@ -0,0 +1,79 @@
+package hote.entity;
+
+
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+
+
+@Entity(name = "User")
+@Table(name = "user") // case sensitive!
+public class User {
+
+
+
+ public String first_name;
+ public String last_name;
+ public String user_name;
+
+    // Every Entity must have a unique identifier which is annotated @Id
+    // Notice there is no @Column here as the column and instance variable name are the same
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+ private int id;
+
+
+
+    public User() {
+    }
+
+    public User(String first_name, String last_name, String user_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.user_name = user_name;
+    }
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", user_name='" + user_name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/hotel/persistence/UserDaoTest.java
@@ -0,0 +1,59 @@
+package hotel.persistence;
+
+import hote.entity.User;
+import hotel.test.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    UserDao dao;
+
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("clean.sql");
+
+
+
+
+        dao = new UserDao();
+    }
+
+
+    @Test
+    void insert() {
+        User newUser = new User("jack", "robertson", "jackrobert");
+        int id = dao.insert(newUser);
+        assertNotEquals(0,id);
+        User insertedUser = dao.getById(id);
+        assertEquals("jack", insertedUser.getFirst_name());
+    }
+
+    @Test
+    void delete() {
+        dao.delete(dao.getById(3));
+        assertNull(dao.getById(3));
+    }
+
+    @Test
+    void getByPropertyLike() {
+
+        List<User> users = dao.getByPropertyLike("last_name", "c");
+        assertEquals(3, users.size());
+    }
+
+    /**
+     * Verify successful retrieval of all users
+     */
+    @Test
+    void getAllSuccess() {
+        List<User> users = dao.getAll();
+        assertEquals(4, users.size());
+    }
+}
