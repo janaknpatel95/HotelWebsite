77519a523bd326cf6f6226ba1982a15cfba1c1d2
Janak Patel <jpatel2@madisoncollege.edu>
2020-10-28 09:59:03 -0500

    this is week 8 done with getting place name from zipcode

---
+++ src/test/java/hotel/persistence/ApiDaoTest.java
@@ -0,0 +1,25 @@
+package hotel.persistence;
+
+import hote.entity.PostalCodesItem;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiDaoTest {
+
+    @Test
+    void getPlace() {
+
+        ApiDao dao = new ApiDao();
+        //System.out.println(dao.getPlace().getPostalCodes());
+        for ( PostalCodesItem code : dao.getPlace().getPostalCodes()){
+            //System.out.println(code.getPlaceName());
+
+            assertEquals("Baraboo", code.getPlaceName());
+        }
+        //System.out.println(code.getPlaceName());
+        //assertEquals("Tatooine", dao.getPlace().getPlaceName().);
+
+
+    }
+}
